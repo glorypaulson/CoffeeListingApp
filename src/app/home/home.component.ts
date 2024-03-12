0eb4e7ab96796947a1c9befe210cd2f6a7a493b0
gloryprobe <glorypaulson85@gmail.com>
2024-03-12 22:03:13 +0530

    some changes extra

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CoffeeService } from '../coffee.service';
-import { Coffee } from '../card/coffe.model';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -9,26 +7,8 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  coffees: Coffee[] = [];
-  available: boolean[] = [];
   constructor(private coffeeService: CoffeeService) {}
   ngOnInit(): void {
-
-    // this.coffeeService.getCoffees().subscribe((coffees) => {
-    //   const availableCoffees = coffees.filter((coffee) => coffee.available);
-    //   console.log(availableCoffees);
-    //   for (let i = 0; i < availableCoffees.length; i++) {
-    //     this.available.push(availableCoffees[i].available);
-    //   }
-    // });
-
-
-    // this.coffeeService.getCoffees().pipe(
-    //   map(coffees => coffees.map(coffee => coffee.available))
-    // ).subscribe(availableValues => {
-    //   // availableValues is an array containing the 'available' property of each coffee object
-    //   console.log(availableValues);
-    // });
 
   }
 }
